628005b31dca27e1ae0e4ed4e48f38ec196b5f2c
Tom Day <tom_day85@hotmail.com>
2020-11-03 11:10:53 +0000

    Fund Performance Graphs

--- src/Pseudonym.Crypto.Invictus.Web/Pseudonym.Crypto.Invictus.Server/Startup.cs
+++ src/Pseudonym.Crypto.Invictus.Web/Pseudonym.Crypto.Invictus.Server/Startup.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
